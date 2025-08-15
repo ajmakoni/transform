5be819e5e6894bcb49924ace28e989feefb667c3
AJ Makoni <ajmakoni@yahoo.com>
2025-08-15 08:52:56 +0200

    actix-web integration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
+use actix_web::{App, get, post, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main()->Result<(), std::io::Error> {
     println!("Hello, world!");
+    todo!()
 }
